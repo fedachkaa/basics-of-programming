4763f4c727d430779a13fad9212cd3132f3ca5ce
Aleksandra Fedak <fedak.aleksandra@student.uzhnu.edu.ua>
2023-04-09 22:52:05 +0300

    Added Verification Email

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\UserAccountController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\UserController;
@@ -32,13 +34,38 @@ Route::post('login', [AuthController::class, 'store'])
     ->name('login.store');
 Route::get('logout', [AuthController::class, 'destroy'])->name('logout');
 
-Route::resource('study', StudySectionController::class);
+Route::middleware(['auth', 'verified'])->group(function () {
 
-Route::get('user', [UserAccountController::class, 'index'])->name('user');
+    Route::resource('study', StudySectionController::class);
 
-Route::get('testing/{id}/{question_id}', [QuestionController::class, 'index'])->name('testing');
-Route::post('testing', [QuestionController::class, 'store'])->name('testing.store');
+    Route::get('user', [UserAccountController::class, 'index'])->name('user');
 
-Route::get('result/{study_section_id}', [QuestionController::class, 'result'])->name('result');
+    Route::get('testing/{id}/{question_id}', [QuestionController::class, 'index'])->name('testing');
+    Route::post('testing', [QuestionController::class, 'store'])->name('testing.store');
+
+    Route::get('result/{study_section_id}', [QuestionController::class, 'result'])->name('result');
+});
 
 Route::get('rating', [IndexController::class, 'rating'])->name('rating');
+
+Route::get('email/verify', function (){
+    return inertia('Auth/VerifyEmail');
+})->middleware('auth')->name('verification.notice');
+
+// VERIFICATION
+Route::get('email/verify', function (){
+    return inertia('Auth/VerifyEmail');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect()->route('home')->with('success', 'Електронну пошту підтверджено!');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return redirect()->route('home')->with('success', 'Посилання для підтвердження електронної пошти надіслано!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
