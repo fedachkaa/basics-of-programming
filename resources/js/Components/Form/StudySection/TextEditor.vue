9806ea55aa7b5cb999c5a6f65303c064537cd59a
Aleksandra Fedak <fedak.aleksandra@student.uzhnu.edu.ua>
2023-05-02 17:06:07 +0300

    Added Text Editor To Admin Panel

---
+++ resources/js/Components/Form/StudySection/TextEditor.vue
@@ -0,0 +1,20 @@
+<template>
+    <Editor v-model="form.content"
+            api-key="14i6t718iubgqiaxrl0ughilerhjn7n7uxvaoj5h7a7hecgu"
+            :init="{
+                plugins: 'lists',
+                toolbar: 'bold | italic | underline | h1 | h2 | h3 | h4 | h5 | h6 | numlist bullist',
+                menubar: false,
+                }"
+    />
+</template>
+
+<script setup>
+import Editor from '@tinymce/tinymce-vue'
+import {defineProps} from "vue";
+
+const props = defineProps({
+    form: Object
+})
+
+</script>
